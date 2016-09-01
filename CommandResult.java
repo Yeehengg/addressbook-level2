dc9d42d8ae88e66ec2c68457a2b4accb20a20b18
Poh Yee Heng <a0125534@u.nus.edu>
2016-09-01 17:28:34 +0800

    Encapsulated CommandResult

--- CommandResult.java
+++ CommandResult.java
@@ -14,7 +14,7 @@ public class CommandResult {
 	 * The feedback message to be shown to the user. Contains a description of
 	 * the execution result
 	 */
-	public static String feedbackToUser;
+	private static String feedBackToUser;
 
 	/** The list of persons that was produced by the command */
 	private final List<? extends ReadOnlyPerson> relevantPersons;
@@ -40,14 +40,14 @@ public class CommandResult {
 	 * Get the feedback to user.
 	 */
 	public String getFeedbackToUser() {
-		return feedbackToUser;
+		return feedBackToUser;
 	}
 
 	/**
 	 * Set the feedback the user gives.
 	 */
 	public void setFeedbackToUser(String feedback) {
-		feedbackToUser = feedback;
+		feedBackToUser = feedback;
 	}
 
 }
